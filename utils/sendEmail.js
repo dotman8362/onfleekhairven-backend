705397797a47dd17aae69952dc21495046db3f3c
dotman8362 <118911062+dotman8362@users.noreply.github.com>
2026-01-29 12:37:48 +0100

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,9 +5,7 @@ import { createEvent } from "ics";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
