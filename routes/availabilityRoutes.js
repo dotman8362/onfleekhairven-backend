15089503c15e128d6e70dd24deec301c79781cc9
dotman8362 <118911062+dotman8362@users.noreply.github.com>
2026-01-29 08:04:43 +0100

    Update availabilityRoutes.js

--- routes/availabilityRoutes.js
+++ routes/availabilityRoutes.js
@@ -3,6 +3,8 @@ import {
   blockDate,
   getBlockedDates,
   unblockDate,
+  blockMultipleDates,
+  unblockMultipleDates
 } from "../controllers/availabilityController.js";
 
 import auth from "../middleware/authMiddleware.js";
@@ -12,6 +14,10 @@ const router = express.Router();
 router.post("/availability/block", auth, blockDate);
 router.get("/availability", getBlockedDates);
 router.delete("/availability/:date", auth, unblockDate);
+
+router.post("/block-dates", blockMultipleDates); // multiple dates
+router.post("/unblock-dates", unblockMultipleDates); // POST body: { dates: ["2026-02-15", "2026-02-16"] }
+
 
 
 export default router;
